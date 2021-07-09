0e117e220c8b2239b6edc6c44ebe8daf44f69a7a
manish591 <manishdevrani777@gmail.com>
2021-07-09 16:31:40 +0530

    test - added test for leap year

--- leap.js
+++ leap.js
@@ -1,6 +1,7 @@
 const birthYear = document.querySelector('.birthyear');
 const calculateBtn = document.querySelector('.calculate');
-const output = document.querySelector('.output');
+const output = document.querySelector('.outputmsg');
+const day = document.querySelector('.day');
 
 function showLeapYear () {
     let inputYear = parseInt(birthYear.value);
@@ -9,6 +10,33 @@ function showLeapYear () {
         return output.innerHTML = 'Please Input your birth year correctly!';
     }
 
+    if (inputYear % 4 === 0) {
+        if (inputYear % 100 === 0) {
+            if (inputYear % 400 === 0) {
+                output.innerText = `Your birth year ${inputYear} is a leap year!`;
+            } else {
+                output.innerText = `Your birth year ${inputYear} is not a leap year!`;
+            }
+        } else {
+            output.innerText = `Your birth year ${inputYear} is a leap year!`;
+        }
+    } else {
+        output.innerText = `Your birth year ${inputYear} is not a leap year!`;
+    }
+
+    birthYear.value = '';
+
 }
 
 calculateBtn.addEventListener('click', showLeapYear);
+
+function dayToNight (e) {
+    document.body.classList.toggle('night');
+    if(e.target.innerText === 'Night') {
+        e.target.innerText = 'Day';
+    } else {
+        e.target.innerText = 'Night'
+    }
+}
+
+day.addEventListener('click', dayToNight);
